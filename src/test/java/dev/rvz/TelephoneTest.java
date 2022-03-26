1a50c44c6488f447b9a04d9dcfa9092e6a9162e2
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-03-26 20:33:33 -0300

    Created test unit telephone value object

---
+++ src/test/java/dev/rvz/TelephoneTest.java
@@ -0,0 +1,30 @@
+package dev.rvz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TelephoneTest {
+
+    @Test
+    void CreateTelephoneWithDDDInvalid() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Telephone(null, "91234-1234"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Telephone("", "91234-1234"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Telephone("000", "91234-1234"));
+    }
+
+    @Test
+    void CreateTelephoneWithNumberInvalid() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Telephone("034", null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Telephone("034", ""));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Telephone("034", "911234-1234"));
+    }
+
+    @Test
+    void createTelephoneWithSuccess() {
+        Telephone telephone = new Telephone("034", "91234-4321");
+        Assertions.assertTrue(telephone.getDdd().equals("034"));
+        Assertions.assertTrue(telephone.getNumber().equals("91234-4321"));
+    }
+}
