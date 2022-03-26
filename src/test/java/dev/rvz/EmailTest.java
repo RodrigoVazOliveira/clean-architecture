1596a7f628603e709427b0bf47e2bb018d234ca3
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-03-26 19:22:35 -0300

    Created test unit to Email value object

---
+++ src/test/java/dev/rvz/EmailTest.java
@@ -0,0 +1,31 @@
+package dev.rvz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    void notCreateEmailInvalid() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Email(null);
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Email("");
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Email("emailinvalido");
+        });
+    }
+
+    @Test
+    void createdWithEmailValid() {
+        Email email = new Email("email@gmail.com");
+
+        Assertions.assertEquals("email@gmail.com", email.getAddress());
+    }
+}
