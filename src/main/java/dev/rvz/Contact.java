e40eaa655311b7744710f3d68c3f2098394d5120
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-03-26 20:32:35 -0300

    Refactoring contact to receive everythings telephones

--- src/main/java/dev/rvz/Contact.java
+++ src/main/java/dev/rvz/Contact.java
@@ -1,27 +1,14 @@
 package dev.rvz;
 
+import java.util.List;
+
 public class Contact {
-    private final String ddd;
-    private final String number;
 
-    public Contact(String ddd, String number) {
-        if (ddd == null || !ddd.matches("^[0][1-9][1-9]$")) {
-            throw new IllegalArgumentException("DDD inputed is invalid!");
-        }
+    private final Email email;
+    private final List<Telephone> telephones;
 
-        if (number == null || !number.matches("^(?:[2-8]|9[1-9])[0-9]{3}\\-[0-9]{4}$")) {
-            throw new IllegalArgumentException("number inputed is invalid!");
-        }
-
-        this.ddd = ddd;
-        this.number = number;
-    }
-
-    public String getDdd() {
-        return ddd;
-    }
-
-    public String getNumber() {
-        return number;
+    public Contact(Email email, List<Telephone> telephones) {
+        this.email = email;
+        this.telephones = telephones;
     }
 }
