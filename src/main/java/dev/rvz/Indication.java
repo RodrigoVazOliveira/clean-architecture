854dbcd994dc0ec8bf2a36aeba23fb783d8dff4f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-03-26 21:59:19 -0300

    Created indicatio

---
+++ src/main/java/dev/rvz/Indication.java
@@ -0,0 +1,12 @@
+package dev.rvz;
+
+public class Indication {
+
+    private final Studant indicated;
+    private final Studant pointer;
+
+    public Indication(Studant indicated, Studant pointer) {
+        this.indicated = indicated;
+        this.pointer = pointer;
+    }
+}
--- src/main/java/dev/rvz/Studant.java
+++ src/main/java/dev/rvz/Studant.java
@@ -1,8 +1,21 @@
 package dev.rvz;
 
+import java.util.List;
+
 public class Studant {
 
-    private CPF CPF;
-    private String name;
-    private Contact contact;
+    private final CPF CPF;
+    private final String name;
+    private final Contact contact;
+
+    public Studant(dev.rvz.CPF CPF, String name, Contact contact) {
+        this.CPF = CPF;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public void addTelephone(String ddd, String number) {
+        List<Telephone> telephones = this.contact.getTelephones();
+        telephones.add(new Telephone(ddd, number));
+    }
 }
