d9a91fc1f5e1d6d065630bccbea02e21e9765517
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-03-26 22:12:42 -0300

    Create Build to Studant

---
+++ src/main/java/dev/rvz/StudantFactory.java
@@ -0,0 +1,34 @@
+package dev.rvz;
+
+import java.util.ArrayList;
+
+public class StudantFactory {
+
+    private Studant studant;
+
+    public StudantFactory createStudanteWithNameAndCPFAndEmail(String name, String CPF, String addressEmail) {
+        CPF cpf = new CPF(CPF);
+        Email email = new Email(addressEmail);
+        Contact contact = new Contact(email, new ArrayList<>());
+
+        this.studant = new Studant(cpf, name, contact);
+
+        return this;
+    }
+
+    public StudantFactory withTelephone(Stirng ddd, String number) {
+        studantIsNull();
+        this.studant.addTelephone(ddd, number);
+        return this;
+    }
+
+    private void studantIsNull() {
+        if (this.studant == null) {
+            throw new NullPointerException("Not created Studant!");
+        }
+    }
+
+    public Studant created() {
+        return studant;
+    }
+}
