6c39fcf35a4ece86dee62d979274f6c5aaef1a23
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-05-12 16:54:48 -0300

    add new event

--- src/main/java/dev/rvz/escola/MatriculateCommandLine.java
+++ src/main/java/dev/rvz/escola/MatriculateCommandLine.java
@@ -1,10 +1,13 @@
-package dev.rvz.escola.academic;
+package dev.rvz.escola;
 
 import dev.rvz.escola.academic.application.matriculate.MatriculateDTO;
 import dev.rvz.escola.academic.application.matriculate.MatriculateService;
 import dev.rvz.escola.academic.domain.student.LogStudentMatriculated;
 import dev.rvz.escola.academic.domain.student.StudantRepository;
 import dev.rvz.escola.academic.infra.studant.StudantRepositoryJDBC;
+import dev.rvz.escola.game.GenerateStampStudentNewber;
+import dev.rvz.escola.game.domain.stamp.StampRepository;
+import dev.rvz.escola.game.infra.stamp.StampRepositoryJDBC;
 import dev.rvz.escola.shared.domain.events.PublishEvents;
 
 import javax.persistence.EntityManager;
@@ -15,9 +18,15 @@ public class MatriculateCommandLine {
     public static void main(String[] args) {
         PublishEvents publishEvents = new PublishEvents();
         publishEvents.add(new LogStudentMatriculated());
+        publishEvents.add(new GenerateStampStudentNewber(getStampRepository()));
 
         MatriculateService matriculateService = new MatriculateService(createStudentRepository(), publishEvents);
         matriculateService.execute(createDTO(args));
+    }
+
+    private static StampRepository getStampRepository() {
+        StampRepository stampRepository = new StampRepositoryJDBC(getEntityManager());
+        return stampRepository;
     }
 
     private static MatriculateDTO createDTO(String[] args) {
@@ -30,10 +39,15 @@ public class MatriculateCommandLine {
     }
 
     private static StudantRepository createStudentRepository() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dev.rvz.escola.DATABASE");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManager entityManager = getEntityManager();
         StudantRepository studantRepository = new StudantRepositoryJDBC(entityManager);
 
         return studantRepository;
+    }
+
+    private static EntityManager getEntityManager() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dev.rvz.escola.DATABASE");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        return entityManager;
     }
 }
